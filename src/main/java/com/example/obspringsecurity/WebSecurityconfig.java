43921306c0d3da684814b2ea55728c5c93c524c3
MarkitosUS <markitosurrea97@gmail.com>
2022-09-28 04:48:40 +0200

    Acualizacion Spring Security

--- src/main/java/com/example/obspringsecurity/WebSecurityconfig.java
+++ src/main/java/com/example/obspringsecurity/WebSecurityconfig.java
@@ -1,9 +1,17 @@
 package com.example.obspringsecurity;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -19,4 +27,27 @@ public class WebSecurityconfig extends WebSecurityConfigurerAdapter {
                 .and().formLogin()
                 .and().httpBasic();
     }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        final User.UserBuilder userBuilder = User.builder().passwordEncoder(encoder::encode);
+        UserDetails user = userBuilder
+                .username("dev")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = userBuilder
+                .username("test")
+                .password("password")
+                .roles("USER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
 }
